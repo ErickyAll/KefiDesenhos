a593e1a80eb7129ea83b70dc725490dc45226946
ErickyAll <erick227.ea21@gmail.com>
2022-09-07 21:12:31 -0300

    menu mobile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from './components/Banner/Banner'
 import Footer from './components/Footer/Footer'
 import Header from './components/header/Header'
 import Section from './components/Section/MoreViews'
+import React from 'react'
 
 function App() {
   return (
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import './Header.css'
 import kefiUserImg from '../../assets/images/iconKefiUser.svg'
 import SearchIcon from '../../assets/images/Search_Icon.svg'
+import './HeaderM.css'
 
 function Header() {
   return (
@@ -8,15 +9,15 @@ function Header() {
       <header>
         <nav>
           <div className="headerDesktop">
-            <p className="textHeader1 textHeader">Sobre</p>
+            <a className="textHeader1 textHeader">Sobre</a>
 
-            <p className="textHeader2 textHeader">KefiDesenhos</p>
+            <a className="textHeader2 textHeader">KefiDesenhos</a>
 
             <div className="userIcons">
               <img alt="Kefi USer" src={kefiUserImg} className="imgIcon" />
             </div>
 
-            <p className="textHeader3 textHeader">Suporte</p>
+            <a className="textHeader3 textHeader">Suporte</a>
 
             <div className="SearchBox">
               <img src={SearchIcon} className="SearchLabel" alt="Lupa" />
@@ -33,11 +34,36 @@ function Header() {
           <div className="wrapper">
             <div className="checkbox">
               <input type="checkbox" id="toggle" />
-              <label className="checkbox" for="toggle">
-                <div className="trace"></div>
-                <div className="trace"></div>
-                <div className="trace"></div>
-              </label>
+              <div className="checkbox-wrapper">
+                <label className="checkbox" for="toggle">
+                  <div className="trace"></div>
+                  <div className="trace"></div>
+                  <div className="trace"></div>
+                </label>
+
+                <header className="headerMobile">
+                  <nav className="menu ">
+                    <div className="SearchBoxM">
+                      <img
+                        src={SearchIcon}
+                        className="SearchLabelM"
+                        alt="Lupa"
+                      />
+
+                      <input
+                        id="searchIptM"
+                        type="search"
+                        className="menu-text"
+                        placeholder="Search..."
+                      />
+                    </div>
+                    <a className="menu-text">Sobre</a>
+                    <a className="menu-text">Inicio</a>
+                    <a className="menu-text">Suporte</a>
+                    <a className="menu-text">Login</a>
+                  </nav>
+                </header>
+              </div>
             </div>
           </div>
         </nav>

575a3fd9d786020a850fcac9358672464d90288b
ErickyAll <erick227.ea21@gmail.com>
2023-03-04 23:16:45 -0300

    formatattion

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -33,17 +33,14 @@ function Banner() {
             Aqui você encontrará uma plataforma fácil e acessível para postar
             seus desenhos e ilustrações online, além de ter a oportunidade de
             interagir com outros artistas e receber feedback sobre seu trabalho.
+            <br />
             No KefiDesenhos, queremos que você se sinta inspirado e motivado a
-            criar cada vez mais. É por isso que encorajamos a diversidade de
-            estilos e técnicas, para que todos possam mostrar seu talento de
-            maneira única e autônoma. Nós acreditamos que a arte é uma forma de
-            expressão poderosa e capaz de conectar pessoas de diferentes
-            culturas e lugares. Por isso, estamos felizes em fornecer uma
-            plataforma onde você pode compartilhar suas ideias e se conectar com
-            outras mentes criativas. Então, não hesite em fazer o upload de suas
-            criações e comece a explorar tudo o que o KefiDesenhos tem a
-            oferecer. Mal podemos esperar para ver o que você tem a compartilhar
-            conosco!
+            criar cada vez mais.
+            <br />
+            Então, não hesite em fazer o upload de suas criações e comece a
+            explorar tudo o que o KefiDesenhos tem a oferecer.
+            <br />
+            Mal podemos esperar para ver o que você tem a compartilhar conosco!
           </p>
         </div>
       </div>
